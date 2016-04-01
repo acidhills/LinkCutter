0eb67196d5645fa878969299b80611aba0dec849
acidhills <acidhills@outlook.com>
2016-04-01 05:26:10 +0400

    Компромис с конфигом мапера

--- LinkCutter/BL/BLAutofacModule.cs
+++ LinkCutter/BL/BLAutofacModule.cs
@@ -7,6 +7,7 @@ using Entities;
 using Entities.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,7 +19,7 @@ namespace BL
         protected override void Load(ContainerBuilder moduleBuilder)
         {
             moduleBuilder.RegisterModule<DALAutofacModule>();
-            moduleBuilder.Register(x =>new LinkToVmMapper("http://localhost:58170/")).As<IVmMapper<Link, LinkVM>>().SingleInstance();
+            moduleBuilder.Register(x =>new LinkToVmMapper(ConfigurationManager.AppSettings["BaseUrl"] ?? "http://localhost:58170/")).As<IVmMapper<Link, LinkVM>>().SingleInstance();
             moduleBuilder.RegisterType<UserToVmMapper>().As<IVmMapper<User, UserVM>>().SingleInstance();
             moduleBuilder.RegisterType<LinkService>().As<ILinkService>().SingleInstance();
             moduleBuilder.RegisterType<UserService>().As<IUserService>().SingleInstance();
